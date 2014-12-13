0e3bd3985e1d08a649d216b2daed7904ebbcd0c9
Ganesh Mahendran <opensource.ganesh@gmail.com>
2014-12-12 16:57:10 -0800

    UPSTREAM: mm/zsmalloc: allocate exactly size of struct zs_pool
    (cherry-pick from commit 181366561ac1e1a7bc3b91dbe45e7614a2f758b9)
    
    In zs_create_pool(), we allocate memory more then sizeof(struct zs_pool)
      ovhd_size = roundup(sizeof(*pool), PAGE_SIZE);
    
    This patch allocate memory of exactly needed size.
    
    Bug: 25951511
    
    Change-Id: Iae1216f7ab304164ea7d687e0037574d6738c94e
    Signed-off-by: Ganesh Mahendran <opensource.ganesh@gmail.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Cc: Dan Streetman <ddstreet@ieee.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -1156,11 +1156,10 @@ static int __init zs_init(void)
  */
 struct zs_pool *zs_create_pool(gfp_t flags, struct zs_ops *ops)
 {
-	int i, ovhd_size;
+	int i;
 	struct zs_pool *pool;
 
-	ovhd_size = roundup(sizeof(*pool), PAGE_SIZE);
-	pool = kzalloc(ovhd_size, GFP_KERNEL);
+	pool = kzalloc(sizeof(*pool), GFP_KERNEL);
 	if (!pool)
 		return NULL;
 
