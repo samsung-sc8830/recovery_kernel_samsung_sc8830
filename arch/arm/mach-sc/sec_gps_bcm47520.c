d37f68565285e31b20784ac0b7a9edb4368ec81e
Dev4Perf <dev4perf@gmail.com>
2018-07-18 16:53:16 +0200

    Fix GPS derps

--- arch/arm/mach-sc/sec_gps_bcm47520.c
+++ arch/arm/mach-sc/sec_gps_bcm47520.c
@@ -24,8 +24,9 @@ static void gps_clk_init(void)
 {
     struct clk *gps_clk;
     struct clk *clk_parent;
+    const char *clk_32k= "clk_aux0";
 
-    gps_clk = clk_get(NULL, "clk_aux0");
+    gps_clk = clk_get(NULL, clk_32k);
     if (IS_ERR(gps_clk)) {
         printk("clock: failed to get clk_aux0\n");
     }
@@ -36,54 +37,53 @@ static void gps_clk_init(void)
 
     clk_set_parent(gps_clk, clk_parent);
     clk_set_rate(gps_clk, 32000);
-    clk_prepare_enable(gps_clk);   
+    if(!clk_prepare_enable(gps_clk))
+        printk("%s: %s enabled!!\n", __func__, clk_32k);
     //clk_enable(gps_clk);
 }
 
-static int gps_enable_control(int flag)
+static int gps_regulator_on(void)
 {
+    int ret = 0;
+    const char *gps_node = "broadcom,bcm47520";
+    struct device_node *root_node = NULL;
     static struct regulator *gps_regulator = NULL;
-    static int f_enabled = 0;
+    const char *reg = NULL;
 
-    pr_info("%s\n", __func__);
-    printk("[GPS] LDO control : %s\n", flag ? "ON" : "OFF");
-
-    if (flag && (!f_enabled)) {
-#if defined(CONFIG_MACH_CORE3_W) || defined(CONFIG_MACH_GOYAVE3G) || defined(CONFIG_MACH_VIVALTO5MVE3G)
-        gps_regulator = regulator_get(NULL, "vddcamd");
-#elif defined(CONFIG_MACH_YOUNG23GDTV) || defined(CONFIG_MACH_J13G)
-        gps_regulator = regulator_get(NULL, "vddcammot");
-#else
-        gps_regulator = regulator_get(NULL, "vddcammot");
-#endif
+    root_node = of_find_compatible_node(NULL, NULL, gps_node);
+    if(!root_node) {
+        WARN(1, "[GPS] failed to get device node of bcm47520\n");
+        return -ENODEV;
+    }
+    /* regulator enable from dts */
+    if(of_property_read_string(root_node, "gps-regulator", &reg)) {
+        pr_err("%s: main(1.8v) regulator field is not exsit\n", __func__);
+        ret = -ENODEV;
+        goto err_find_node;
+    } else {
+        gps_regulator = regulator_get(NULL, reg);
         if (IS_ERR(gps_regulator)){
-             gps_regulator = NULL;
-             return EIO;
-        } else {
-             regulator_set_voltage(gps_regulator, 1800000, 1800000);
-             regulator_enable(gps_regulator);
-        }
-        f_enabled = 1;
-    }
-    if (f_enabled && (!flag))
-    {
-        if (gps_regulator) {
-            regulator_disable(gps_regulator);
-            regulator_put(gps_regulator);
             gps_regulator = NULL;
+            return -EIO;
+        } else {
+            regulator_set_voltage(gps_regulator, 1800000, 1800000);
+            if(!regulator_enable(gps_regulator))
+                printk("%s: (%s) main(1.8v) regulator turned ON!!\n", __func__, reg);
         }
-        f_enabled = 0;
     }
-
     return 0;
-}
 
+err_find_node:
+    of_node_put(root_node);
+    return ret;
+}
 
 static unsigned int gps_pwr_on = 0;
 
 static int __init gps_bcm47520_init(void)
 {
-    gps_enable_control(1);
+    pr_info("%s\n", __func__);
+    gps_regulator_on();
     gps_clk_init();
 
     int ret = 0;
@@ -101,17 +101,17 @@ static int __init gps_bcm47520_init(void)
         ret = -ENODEV;
         goto err_sec_device_create;
     }
-       
+
     gps_pwr_on = of_get_named_gpio(root_node, gps_pwr_en, 0);
     if(!gpio_is_valid(gps_pwr_on)) {
         WARN(1, "Invalid gpio pin : %d\n", gps_pwr_on);
         ret = -ENODEV;
-        goto err_sec_device_create;
+        goto err_find_node;
     }
     if (gpio_request(gps_pwr_on, "GPS_PWR_EN")) {
         WARN(1, "fail to request gpio(GPS_PWR_EN)\n");
         ret = -ENODEV;
-        goto err_sec_device_create;
+        goto err_find_node;
     }
     gpio_direction_output(gps_pwr_on, 0);
     gpio_export(gps_pwr_on, 1);
@@ -119,6 +119,8 @@ static int __init gps_bcm47520_init(void)
 
     return 0;
 
+err_find_node:
+    of_node_put(root_node);
 err_sec_device_create:
     device_destroy(gps_dev, gps_dev->devt);
     return ret;
